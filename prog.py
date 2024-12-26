8d94cebec94f4f9597996404da2f534bc677edc4
Danila <tut.fenfen@mail.ru>
2024-12-26 23:07:30 +0300

    Изменение программы.

--- prog.py
+++ prog.py
@@ -1,7 +1,18 @@
-a = int(input('Введите	свой	возраст:	'))
-if	a	>=	21:
- print('Доступ	разрешен')
-elif	a	>=	18:
- print('Доступ	частично	разрешен')
+import	random
+number	=	random.randint(1,	25)
+choices	=	0
+while	choices	<	5:
+    print('Угадай	число	между	1	and	25:')
+    guess = input()
+    guess = int(guess)
+    choices	=	choices	+	1
+    if	guess	<	number:	#если	число	пользователя	меньше	загаданного
+        print ('Мое	число	больше	твоего')
+    if	guess	>	number:	#если	число	пользователя	больше	загаданного
+        print ('Мое	число	меньше	твоего')
+    if	guess	==	number:	#если	число	пользователя	равно	загаданному 
+       break
+if	guess	==	number:
+ print('Молодец!	Ты	угадал	число	с'	+	str(choices)	+	'попытки!')
 else:
- print('Доступ	запрещен')
+ print('К	сожалению,	ты	не	угадал	число.	Я	загадал'	+	str(number))
